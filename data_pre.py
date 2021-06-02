9b94ab05cab46b266307e4fd1c6bbbb5e1befe60
lonless0 <69523833+lonless0@users.noreply.github.com>
2021-06-02 10:39:20 +0800

    Add files via upload

---
+++ data_pre.py
@@ -0,0 +1,19 @@
+import csv
+import os
+
+from tqdm import tqdm
+
+f = open('财经.csv','w',encoding='utf-8')
+csv_writer = csv.writer(f)
+csv_writer.writerow(["file_name","class_name","text"])
+file_path = '分类样本集'
+class_name = '财经'
+rootdir = '分类样本集/财经'
+list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
+for i in tqdm(range(0,len(list))):
+    path = os.path.join(rootdir, list[i])
+    with open(path, "r") as df:
+        text=df.read().replace('\n','').replace('　','').strip()
+    csv_writer.writerow([list[i],class_name,text])
+    df.close()
+f.close()
